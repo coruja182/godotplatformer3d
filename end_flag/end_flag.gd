973dadddcb821658394b8ef29faa98da2912fe07
Luis Henrique Rocha <henrique.srocha@gmail.com>
2024-12-30 02:50:20 +0100

    feat: End Flag

---
+++ end_flag/end_flag.gd
@@ -0,0 +1,13 @@
+class_name EndFlag extends Area3D
+
+
+@export_file("*.tscn") var next_scene
+
+
+func _ready() -> void:
+	assert(next_scene, "next scene is mandatory")
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.is_in_group("Player"):
+		get_tree().change_scene_to_file(next_scene)
